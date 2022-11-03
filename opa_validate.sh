54e99c741099b35654bc33d19eb00317abe46098
Simon Duff <simon.duff@gmail.com>
2022-11-03 17:36:54 +1100

    Create opa_validate.sh

---
+++ opa_validate.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+opa_output=$( opa eval --data policy.rego --input example.tfplan.json --format pretty "data.confluent.deny" 2>&1 )
+echo $opa_output
+
+if [ "$opa_output" == "[]" ]; then
+  # no deny, continue
+  exit 0
+else
+  # deny, fail
+  exit 1
+fi
+
