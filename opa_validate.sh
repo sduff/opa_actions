aad012d58d3bb0596a87428818f624e353e86101
Simon Duff <simon.duff@gmail.com>
2022-11-04 13:19:29 +1100

    Update opa_validate.sh

--- opa_validate.sh
+++ opa_validate.sh
@@ -3,7 +3,7 @@
 opa_output=$( opa eval --data policies/policy.rego --input example.tfplan.json --format pretty "data.confluent.deny" 2>&1 )
 echo "$opa_output"
 
-if [ "$opa_output" == "[]" ]; then
+if [ "$opa_output" = "[]" ]; then
   # no deny, continue
   exit 0
 else
